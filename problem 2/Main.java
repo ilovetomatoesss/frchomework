24a313080607060246e1d6f8bee770844c819708
ilovetomatoesss <michelleshim3@gmail.com>
2025-01-07 15:42:54 -0800

    finished problem 2

---
+++ problem 2/Main.java
@@ -0,0 +1,33 @@
+class Main {
+
+    public static int addNums(int x, int y){
+        return (x+y);
+
+    }
+
+    public static int subtractNums(int x, int y){
+        return (x-y);
+        
+    }
+
+    public static double divideNums(double x, double y){
+        return (x/y);
+        
+    }
+
+    public static int multiplyNums(int x, int y){
+        return (x*y);
+        
+    }
+
+    public static void main(String[] args) { 
+        System.out.println(addNums(70,7));
+        System.out.println(subtractNums(70,7));
+        System.out.println(divideNums(70,7));
+        System.out.println(multiplyNums(70,7));
+
+    }
+
+
+
+}
