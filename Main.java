16baae8c6682e12b80309a8b89c7a1b86638995b
Michelle Shim <michelleshim3@gmail.com>
2025-01-06 20:58:15 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,22 @@
 class Main {
+  
   public static void main (String[] args) {
+    //created first object
     Person michelle = new Person();
-
     System.out.println(michelle);
     michelle.setAge(15);
     michelle.setName("michelle");
     michelle.setHeight(6);
     michelle.setHairColor("black");
+    System.out.println(michelle);
+
+    // created second object
+    Person steve = new Person();
+    System.out.println(steve);
+    steve.setAge(20000);
+    steve.setName("steve");
+    steve.setHeight(6.2);
+    steve.setHairColor("dark brown");
   }
-setAge (
 
   }
