759eed650ad16e2b4aafbf00516056301a41acda
ilovetomatoesss <michelleshim3@gmail.com>
2025-01-08 15:40:03 -0800

    made some progress

---
+++ homework #1/Main.java
@@ -0,0 +1,11 @@
+class Main {
+    int grade = 102.5;
+    boolean icecream = true;
+
+    System.out.println("Hello")
+    System.out.println("Michelle Shim")
+    System.out.println(grade)
+
+
+
+}
