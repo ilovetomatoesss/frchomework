8f0554e8c37c74ec113cf8141cd26eb103e8cc4b
Michelle Shim <michelleshim3@gmail.com>
2025-01-11 16:26:28 -0800

    did up to exercise 5

--- homework #1/Main.java
+++ homework #1/Main.java
@@ -1,11 +1,40 @@
 class Main {
+    
     int grade = 102.5;
     boolean icecream = true;
+    int FavNum = 7;
+    int test1 = 83;
+    int test2 = 25;
+    int test3 = 98;
 
+    // exercise 1
     System.out.println("Hello")
     System.out.println("Michelle Shim")
+    
+    // exercise 2
     System.out.println(grade)
 
+    public String toString(){
+        return "My math grade is " grade ".";}
+    
+    // exercise 3
+    public String toString(){
+        return "The statement I like ice cream is " icecream "."}
 
+    // exercise 4
+    FavNum = Favnum + 10;
+    FavNum = Favnum - 14;
+    FavNum = Favnum * 2;
+    FavNum = Favnum / 3;
+    System.out.println(FavNum);
+
+    // exercise 5
+
+    double average = (test 1 + test 2 + test 3) / 3.0;
+    public String toString(){
+        return "Rachel's average test score is " average ".";}
+    
+
+    // exercise 6
 
 }
