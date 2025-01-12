ad78c1715a0f6dae0b167cb1ed3eb9c1de361338
Michelle Shim <michelleshim3@gmail.com>
2025-01-11 18:01:15 -0800

    Update and rename Main.java to NumberOne.java

---
+++ homework #3/NumberOne.java
@@ -0,0 +1,19 @@
+class NumberOne{
+
+  // exercise 1
+  int number = 5;
+
+  int 
+  if (number > 0) {
+  System.out.println("The number is " + number + "is positive.");
+  }
+  else if (number < 0) {
+  System.out.println("The number is " + number + "is negative.")
+  }
+  else {
+  System.out.println("The number is " + number + "is zero.")
+  }
+
+
+  
+}
