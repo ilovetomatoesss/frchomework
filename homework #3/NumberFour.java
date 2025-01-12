89f7f6f89e0f7ea8e9c55671da3bd5b09b3638c3
Michelle Shim <michelleshim3@gmail.com>
2025-01-11 18:30:27 -0800

    Create NumberFour.java

---
+++ homework #3/NumberFour.java
@@ -0,0 +1,13 @@
+class NumberFour {
+  int first = 3;
+  int second = 5;
+
+    if (first > second){
+    System.out.println("The number" + first + "is bigger than the number" + second + ".");
+    }
+    else if (second > first){
+    System.out.println("The number" + second + "is bigger than the number" + first + ".");
+    }
+      
+
+}
