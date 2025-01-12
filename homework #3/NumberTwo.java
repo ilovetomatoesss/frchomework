05e60e4680dae405311d4dcf941ee5eee82c97f8
Michelle Shim <michelleshim3@gmail.com>
2025-01-11 18:21:14 -0800

    Create NumberTwo.java

---
+++ homework #3/NumberTwo.java
@@ -0,0 +1,15 @@
+class NumberTwo {
+
+  int year = 2004;
+
+    if (year/4) {
+      System.out.println("The year is " + number + "a leap year.");
+    }
+    else if (year<0) {
+      System.out.println("The number given " + number + "is not a valid year.");
+    }
+    else {
+      System.out.println("The number given " + number + "is not a leap year.");
+    }
+  
+}
