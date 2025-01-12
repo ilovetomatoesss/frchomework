b3d8ce7cea96d9566aa9e1474478135f1edfed10
Michelle Shim <michelleshim3@gmail.com>
2025-01-11 17:47:14 -0800

    Update Main.java

--- homework #1/Main.java
+++ homework #1/Main.java
@@ -10,21 +10,17 @@ class Main {
     int test2 = 25;
     int test3 = 98;
     // exercise 8
-    String favSubject = "math";
+    String FavSubject = "math";
 
     // exercise 1
-    System.out.println("Hello")
-    System.out.println("Michelle Shim")
+    System.out.println("Hello");
+    System.out.println("Michelle Shim");
     
     // exercise 2
-    System.out.println(grade)
+    System.out.println("My math grade is "  + grade + ".");
 
-    public String toString(){
-        return "My math grade is "  + grade ".";}
-    
     // exercise 3
-    public String toString(){
-        return "The statement I like ice cream is " + icecream ".";}
+    System.out.println("The statement I like ice cream is " + icecream + ".");
 
     // exercise 4
     FavNum = Favnum + 10;
@@ -36,8 +32,7 @@ class Main {
     // exercise 5
 
     double average = (test 1 + test 2 + test 3) / 3.0;
-    public String toString(){
-        return "Rachel's average test score is " average ".";}
+    System.out.println("Rachel's average test score is " + average + ".");
     
 
     // exercise 6
@@ -49,7 +44,6 @@ class Main {
     System.out.print("Yes, No, 10th grade!")
 
     // exercerise 8
-    public String toString(){
-        return "My phavorite subject is " + favSubject ".";}
+    System.out.println("My phavorite subject is " +  FavSubject + ".";}
             
         }
